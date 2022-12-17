d74503216b82aacdb7949f0156446f926ee43e86
Simon-Muchemi <muchemi.simon21@students.dkut.ac.ke>
2022-12-17 20:26:34 +0300

    Day 3: LinkedLists: added indexof(), addLast(), addFirst() successfully.

--- src/LinkedLists.java
+++ src/LinkedLists.java
@@ -1,4 +1,61 @@
 public class LinkedLists {
+    private int flag = 0;
+    private int itemIndex = -1;
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+
     private Node first;
     private Node last;
+
+    public void addLast(int item){
+        var node = new Node(item);
+
+        if (isEmpty()){
+            first = last = node;
+        }
+        else {
+            last.next = node;
+            last = node;
+        }
+    }
+    public void addFirst(int item){
+        var node = new Node(item);
+        if (isEmpty()){
+            first = last = node;
+        }
+        else{
+            node.next = first;
+            first = node;
+        }
+    }
+    private boolean isEmpty(){
+        return first == null;
+    }
+
+    public void indexOf(int item){
+        int index = 0;
+        var current = first;
+        while(current!= null){
+            if (current.value==item) {
+                System.out.println(index);
+                flag = 1;
+                itemIndex = index;
+            }
+            current = current.next;
+            ++index;
+        }
+        if (flag==0) System.out.println(-1);
+
+        //System.out.println(-1);
+    }
+    /* public boolean contains(int item){
+        indexOf(item);
+    }*/
 }
